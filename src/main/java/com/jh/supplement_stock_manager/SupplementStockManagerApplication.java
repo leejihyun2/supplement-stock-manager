2431fbde0dc4daeec055b7e32aa6e9ce213376bb
leejh <leejxhxxx0159@gmail.com>
2026-08-01 17:47:47 +0900

    chore: 스프링 부트 프로젝트 초기 설정

---
+++ src/main/java/com/jh/supplement_stock_manager/SupplementStockManagerApplication.java
@@ -0,0 +1,13 @@
+package com.jh.supplement_stock_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplementStockManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplementStockManagerApplication.class, args);
+	}
+
+}
