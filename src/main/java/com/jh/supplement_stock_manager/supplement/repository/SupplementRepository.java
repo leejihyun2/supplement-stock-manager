bd2bf96c80d54b766eb236e86b014973731b98a3
leejh <leejxhxxx0159@gmail.com>
2026-08-08 22:45:22 +0900

    feat: 영양제 엔티티 및 저장소 구현

---
+++ src/main/java/com/jh/supplement_stock_manager/supplement/repository/SupplementRepository.java
@@ -0,0 +1,11 @@
+package com.jh.supplement_stock_manager.supplement.repository;
+
+import com.jh.supplement_stock_manager.supplement.entity.Supplement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupplementRepository extends JpaRepository<Supplement, Long> {
+    //Supplement는 이 Repository가 관리할 엔티티
+    //Long은 Supplement의 PK인 supplementId의 타입
+}
+
+//Repository: 엔티티를 이용해서 DB의 데이터를 저장,조회,수정,삭제할 수 있도록 JPA와 연결해주는 계층
